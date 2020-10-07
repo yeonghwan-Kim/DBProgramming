aacd9210c5e6e53d0901af795b2d75d3d48ce401
yeonghwan-Kim <72487379+yeonghwan-Kim@users.noreply.github.com>
2020-10-07 16:24:15 +0900

    Add files via upload
    Github 최초 활용

---
+++ MarketBrowser/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MarketBrowser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenCSV = new System.Windows.Forms.Button();
+            this.textBoxCSVViewer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOpenCSV
+            // 
+            this.buttonOpenCSV.Location = new System.Drawing.Point(21, 23);
+            this.buttonOpenCSV.Name = "buttonOpenCSV";
+            this.buttonOpenCSV.Size = new System.Drawing.Size(70, 23);
+            this.buttonOpenCSV.TabIndex = 0;
+            this.buttonOpenCSV.Text = "CSV 열기";
+            this.buttonOpenCSV.UseVisualStyleBackColor = true;
+            this.buttonOpenCSV.Click += new System.EventHandler(this.buttonOpenCSV_Click);
+            // 
+            // textBoxCSVViewer
+            // 
+            this.textBoxCSVViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCSVViewer.Location = new System.Drawing.Point(12, 80);
+            this.textBoxCSVViewer.Multiline = true;
+            this.textBoxCSVViewer.Name = "textBoxCSVViewer";
+            this.textBoxCSVViewer.Size = new System.Drawing.Size(592, 358);
+            this.textBoxCSVViewer.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCSVViewer);
+            this.Controls.Add(this.buttonOpenCSV);
+            this.Name = "MainForm";
+            this.Text = "전통시장 검색기";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenCSV;
+        private System.Windows.Forms.TextBox textBoxCSVViewer;
+    }
+}
+
---
+++ MarketBrowser/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketBrowser
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenCSV_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(new FileStream("market.csv", FileMode.Open), Encoding.Default);
+
+            string str = "";
+            var line = sr.ReadLine();
+            var headers = line.Split(',');
+
+            foreach(string header in headers)
+            {
+                textBoxCSVViewer.Text += header + "\t";
+            }
+            /*
+            while (sr.EndOfStream == false)
+            {
+                line = sr.ReadLine();
+                var values = line.Split(',');
+                str += values[0] + "\r\n";
+            }
+
+            textBoxCSVViewer.Text = str;
+            */
+            sr.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
